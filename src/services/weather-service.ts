032ad3d02833314e7728f8bcc546c4afe7957f92
Michael Paulinus <michaelpaulinus@gmail.com>
2025-03-31 17:21:06 +0200

    Removed api keys

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,3 +1,11 @@
-class WeatherService {}
+class WeatherService {
+	async getCurrentWeather(location: string) {
+		return fetch('api/weather/current');
+	}
+
+	getForecastedWeather(location: string) {
+		return fetch('api/weather/forecast');
+	}
+}
 
 export default new WeatherService();
