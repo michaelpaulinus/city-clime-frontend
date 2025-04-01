4333e18f37d5b064d9609f902f5425c413bb3b9d
Michael Paulinus <michaelpaulinus@gmail.com>
2025-04-01 17:03:47 +0200

    Updated cities

--- src/app/data/cities.ts
+++ src/app/data/cities.ts
@@ -1,2 +1,41 @@
-// export default ['Johannesburg', 'London', 'New York', 'Sydney', 'Tokyo'];
-export default ['Johannesburg'];
+export default [
+	// Africa (10 cities)
+	'Cape Town',
+	'Johannesburg',
+	'Cairo',
+	'Nairobi',
+	'Lagos',
+	'Casablanca',
+	'Accra',
+	'Addis Ababa',
+	'Dar es Salaam',
+	'Kigali',
+
+	// Americas (5 cities)
+	'New York',
+	'Los Angeles',
+	'Toronto',
+	'Rio de Janeiro',
+	'Mexico City',
+
+	// Asia & Middle East (8 cities)
+	'Tokyo',
+	'Dubai',
+	'Singapore',
+	'Bangkok',
+	'Hong Kong',
+	'Seoul',
+	'Shanghai',
+	'Istanbul',
+
+	// Europe (5 cities)
+	'London',
+	'Paris',
+	'Berlin',
+	'Rome',
+	'Barcelona',
+
+	// Oceania (2 cities)
+	'Sydney',
+	'Melbourne',
+];
