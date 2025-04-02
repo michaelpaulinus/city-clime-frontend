993424a74090e101f0919ff297cafaa1e0937195
Michael Paulinus <michaelpaulinus@gmail.com>
2025-04-02 16:47:56 +0200

    Removed cities

--- src/app/data/cities.ts
+++ src/app/data/cities.ts
@@ -1,41 +1,8 @@
 export default [
-	// Africa (10 cities)
-	'Cape Town',
+	'Durban',
 	'Johannesburg',
-	'Cairo',
-	'Nairobi',
 	'Lagos',
-	'Casablanca',
-	'Accra',
-	'Addis Ababa',
-	'Dar es Salaam',
-	'Kigali',
-
-	// Americas (5 cities)
 	'New York',
-	'Los Angeles',
-	'Toronto',
-	'Rio de Janeiro',
-	'Mexico City',
-
-	// Asia & Middle East (8 cities)
 	'Tokyo',
-	'Dubai',
-	'Singapore',
-	'Bangkok',
-	'Hong Kong',
-	'Seoul',
-	'Shanghai',
-	'Istanbul',
-
-	// Europe (5 cities)
 	'London',
-	'Paris',
-	'Berlin',
-	'Rome',
-	'Barcelona',
-
-	// Oceania (2 cities)
-	'Sydney',
-	'Melbourne',
 ];
