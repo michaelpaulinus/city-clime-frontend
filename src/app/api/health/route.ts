d7b159d5b7b8c2652cc9ab8f0d522d5ceaea48d8
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-24 21:48:21 +0200

    Initial commit

--- src/app/api/health/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-	try {
-		return NextResponse.json({ status: 200 });
-	} catch (error) {
-		return NextResponse.json({ status: 500, error: error });
-	}
-}
--- src/models/current-weather.ts
+++
@@ -1,7 +0,0 @@
-import Location from './location';
-import Current from './current';
-
-export default interface CurrentWeather {
-	location: Location;
-	current: Current;
-}
