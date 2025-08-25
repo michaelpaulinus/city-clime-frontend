a2e855d9ff81a4cfdd6dac0abf80c6dbd5283980
= <=>
2025-08-25 14:59:21 +0200

    Updated .env variable

--- src/configs/http-client.ts
+++ src/configs/http-client.ts
@@ -1,6 +1,6 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
 
-const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_BASE_URL;
+const WEATHER_API_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 const configAPI: CreateAxiosDefaults = {
   baseURL: `${WEATHER_API_URL}`,
