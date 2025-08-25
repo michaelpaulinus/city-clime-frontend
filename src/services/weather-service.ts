ac238f1639e2e7169e269b6a021862e1227603f0
= <=>
2025-08-25 02:05:18 +0200

    Removed 'getCurrentWeather' function

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,14 +1,9 @@
-import type CurrentWeather from '../types/current-weather';
 import type ForecastWeather from '../types/forecast-weather';
 import httpClient from '../configs/http-client';
 
 class WeatherService {
-  async getCurrentWeather(location: string) {
-    return httpClient.get<CurrentWeather>(`$/weather/current/${location}`);
-  }
-
-  async getForecastedWeather(location: string) {
-    return httpClient.get<ForecastWeather>(`/weather/forecast/${location}`);
+  async getForecastedWeather() {
+    return httpClient.get<ForecastWeather>(`/weather/forecast`);
   }
 }
 
